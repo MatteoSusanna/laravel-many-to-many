f78a0bfa0fdf5b20c1f4320ada4d700ecd2c4b17
Matteo Susanna <matteo2784@gmail.com>
2022-10-13 14:13:06 +0200

    Fine esercitazione

--- resources/views/admin/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Home Amministratore')
-
-@section('content')
-    <div class="container">
-        <h1>Benvenuto {{Auth::user()->name}}</h1>
-    </div>
-@endsection
